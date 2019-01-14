85f6a52aac365af9e9d01bc2bb9a7303b86ba281
Ian Bury <ianbury@mbp-ibury2.local>
2019-01-14 15:49:20 -0800

    testing function

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask
 import os
+#delete from here
+import sys
+from py_google_fit.GoogleFit import GFitDataType
+from py_google_fit.GoogleFit import GoogleFit
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	return (os.environ['dbname'])
+	client_id = os.environ['client_id']
+	client_secret = os.environ['client_secret']
+	fit = GoogleFit(client_id, client_secret)
+	fit.authenticate()
+	return fit.average_today(GFitDataType.STEPS)
 
 if __name__ == '__main__':
     app.run()
--- py_google_fit/main.py
+++ py_google_fit/main.py
@@ -6,8 +6,8 @@ except:
 	pass
 from py_google_fit.GoogleFit import GFitDataType
 from py_google_fit.GoogleFit import GoogleFit
-client_id = os.environ.API_CONFIG['client_id']
-client_secret = os.environ.API_CONFIG['client_secret']
+client_id = os.environ['client_id']
+client_secret = os.environ['client_secret']
 fit = GoogleFit(client_id, client_secret)
 fit.authenticate()
 fit.average_by_hour_n_days_ago(GFitDataType.STEPS,0,0)
