48ec4a1ae78de989534d376495a5f0d1cd63ab88
Ian Bury <ianbury@mbp-ibury2.local>
2019-01-14 15:56:31 -0800

    none

--- py_google_fit/main.py
+++ py_google_fit/main.py
@@ -1,9 +1,4 @@
 import sys
-#import config
-#try:
-#	sys.path.insert(0,os.environ.API_CONFIG['local_path'])
-#except:
-#	pass
 from py_google_fit.GoogleFit import GFitDataType
 from py_google_fit.GoogleFit import GoogleFit
 client_id = os.environ['client_id']
