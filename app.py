68582729616497c78c045e7fc4a5f0c54d18b5e1
Ian Bury <ianbury@mbp-ibury2.local>
2019-01-14 19:28:51 -0800

    na

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 import os
 #delete from here
 import sys
-from GoogleFit import GFitDataType
-from GoogleFit import GoogleFit
+from py_google_fit.GoogleFit import GFitDataType
+from py_google_fit.GoogleFit import GoogleFit
 
 app = Flask(__name__)
 
@@ -14,7 +14,7 @@ def hello_world():
 	fit = GoogleFit(client_id, client_secret)
 	fit.authenticate()
 	#fit.authenticate should only store credentials on s3, no need to retrieve them.
-	return average_by_hour_n_days_ago(GFitDataType.STEPS,1,0)
+	return fit.average_by_hour_n_days_ago(GFitDataType.STEPS,1,0)
 
 if __name__ == '__main__':
     app.run()
